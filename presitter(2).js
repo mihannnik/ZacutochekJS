11ba68e337c03edb53071a995005fc3ac8feffa6
Mikhail Grekov <48869235+mihannnik@users.noreply.github.com>
2020-08-02 22:54:37 +0500

    Update presitter(2).js

--- presitter(2).js
+++ presitter(2).js
@@ -2,7 +2,7 @@
 
 // @name         PreSitter(2)
 // @namespace    http://tampermonkey.net/
-// @version      2
+// @version      3
 
 // @updateURL    https://raw.githubusercontent.com/mihannnik/ZacutochekJS/master/presitter(2).js#bypass=true
 // @downloadURL  https://raw.githubusercontent.com/mihannnik/ZacutochekJS/master/presitter(2).js#bypass=true
