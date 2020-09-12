8653fe4960da5b3384e6fd6851060ef34ce5fbc4
Mikhail Grekov <48869235+mihannnik@users.noreply.github.com>
2020-09-12 15:40:40 +0500

    Create GoogleToShenpoker.js

---
+++ GoogleToShenpoker.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         GoogleToShenpoker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://google.com/
+// @grant        GM_openInTab
+// @grant        window.close
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_openInTab("https://shenpoker.com/cn",'active');
+    // Your code here...
+})();
