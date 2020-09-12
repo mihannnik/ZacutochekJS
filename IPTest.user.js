583468f6fd382927f7973c60cb5133af2e08ee7e
Mikhail Grekov <48869235+mihannnik@users.noreply.github.com>
2020-09-12 15:39:06 +0500

    Update IPTest.user.js

--- IPTest.user.js
+++ IPTest.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         IPTest
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1
 // @description  try to take over the world!
 // @author       You
 // @match        https://2ip.ru/
@@ -13,9 +13,9 @@
     'use strict';
 
     var elem = document.getElementById("d_clip_button").children[0];
-    if(elem.innerText != "217.8.127.18")
+    if(elem.innerText == "")
     {
-    GM_openInTab("http://shenpoker.com/cn");
+    GM_openInTab("https://google.com/",'active');
         self.close();
     }
 
