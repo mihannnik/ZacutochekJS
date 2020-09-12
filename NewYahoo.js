d1d32d78b0ded5cbda7de8b7d73e6289ab34406a
Mikhail Grekov <48869235+mihannnik@users.noreply.github.com>
2020-09-12 16:30:44 +0500

    Create NewYahoo.js

---
+++ NewYahoo.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         GoogleToShenpoker
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://in.yahoo.com/?p=us
+// @grant        GM_openInTab
+// @grant        window.close
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_openInTab("https://shenpoker.com/cn",'active');
+    self.close();
+
+})();
