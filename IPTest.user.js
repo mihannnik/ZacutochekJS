3821d7ab29f0dfe98f1f9cfd5b2db2da600a1283
Mikhail Grekov <48869235+mihannnik@users.noreply.github.com>
2020-10-16 00:02:35 +0500

    Update IPTest.user.js

--- IPTest.user.js
+++ IPTest.user.js
@@ -10,7 +10,7 @@
 // @run-at       document-end
 // ==/UserScript==
 
-(function() {
+(async function() {
     'use strict';
 
         var elem = document.getElementById("d_clip_button").children[0];
@@ -21,7 +21,13 @@
     }
     else if(elem.innerText == "217.8.127.18")
     {
+        await sleep(1000);
         location.reload();
     }
     self.close();
 })();
+
+function sleep(ms)
+{
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
